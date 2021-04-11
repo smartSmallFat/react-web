7c425cea4f286e88b4631c00e96cb13eb89bcd42
smartSmallFat <2425832348@qq.com>
2021-04-11 19:06:46 +0800

    下载axios，在src下面创建setupProxy.js配置跨域

---
+++ src/setupProxy.js
@@ -0,0 +1,21 @@
+const proxy = require('http-proxy-middleware')
+module.exports = function (app) {
+    app.use(
+        proxy('/api', {
+            target: 'http://xxxxx.com',
+            changeOrigin: true,
+            // pathRewrite: {
+            //     '^/api': ''
+            // }
+        }),
+        /*
+        //可以写很多代理
+        proxy('/api1', {
+            target: 'http://xxxxxx.com',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': ''
+            }
+        })*/
+    )
+}
